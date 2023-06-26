68f3d433f4b4fefe6f292f1c88751d82ef66557b
shjune0411 <shjune0411@naver.com>
2023-06-26 16:03:17 +0900

    정적 대시보드 제작

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-function App() {
+const data = [
+    {
+        "name" : "similarity",
+        "source" : 100,
+        "lic": 80
+    }
+];
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div style={{ flex: 1, marginLeft: '20px' }}>
+              <ResponsiveContainer width={400} height={300}>
+                  <BarChart width={500} height={200} data={data} barCategoryGap={50} barGap={10}>
+                      <CartesianGrid strokeDasharray="3 3" />
+                      <XAxis dataKey="name" />
+                      <YAxis />
+                      <Tooltip />
+                      <Legend />
+                      <Bar dataKey="source" fill="black" barSize={50} />
+                      <Bar dataKey="lic" fill="blue" barSize={50} />
+                  </BarChart>
+              </ResponsiveContainer>
+          </div>
+          <div style={{ flex: 1, marginLeft: '-37%' }}>
+              <h4 style={{ marginTop: '10px',  display: 'inline-block', alignItems: 'center', width: '100%', justifyContent: 'center', whiteSpace: 'pre' }}>
+                  <span>user code{'\n'}</span>
+                  <span style={{ backgroundColor: "white" }}>
+                    main(){'{'} {"\n"}
+                    </span>
+                  <span style={{ backgroundColor: "red" }}>printf("hello world");{'\n'}</span>
+                  <span style={{ backgroundColor: "white" }}>{'}'}</span>
+              </h4>
+          </div>
+          <div style={{ flex: 1, marginLeft: '-40%' }}>
+              <h4 style={{ marginTop: '10px',  display: 'inline-block', alignItems: 'center', width: '100%', justifyContent: 'center', whiteSpace: 'pre' }}>
+                  <span>licence code {'\n'}</span>
+                  <span style={{ backgroundColor: "white" }}>
+                    main(){'{'} {"\n"}
+                    </span>
+                  <span style={{ backgroundColor: "red" }}>printf("hello world");{'\n'}</span>
+                  <span style={{ backgroundColor: "white" }}>{'}'}</span>
+              </h4>
+          </div>
+      </div>
   );
 }
-
-export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React, { PureComponent } from 'react';
+import { BarChart, Bar,
+         Cell, XAxis,
+         YAxis, CartesianGrid,
+         Tooltip, Legend,
+         ResponsiveContainer } from 'recharts'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const data = [
+    {
+        "name" : "similarity",
+        "sc" : 100,
+        "lic": 80
+    }
+]
+
+export default class Example extends PureComponent {
+    static demoUrl = 'https://codesandbox.io/s/tiny-bar-chart-35meb';
+  
+    render() {
+      return (
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart width={150} height={40} data={data}>
+            <Bar dataKey="uv" fill="#8884d8" />
+          </BarChart>
+        </ResponsiveContainer>
+      );
+    }
+  }
